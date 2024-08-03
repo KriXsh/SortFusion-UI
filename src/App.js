cd34d5599cd9cc67a59d6f1ecdd4da888bf37fd7
krish <krishnendughosal999@gmail.com>
2024-08-03 12:24:06 +0530

    remove particle.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 // src/App.js
 import React from 'react';
 import './App.css';
-import ParticlesBackground from './components/ParticlesBackground';
 import SortVisualizer from './components/SortVisualizer';
 
 function App() {
-  return (
-    <div className="App">
-      {/* <ParticlesBackground /> */}
-      <div className="sort-container">
-        <SortVisualizer />
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            
+            <div className="sort-container">
+                <SortVisualizer />
+            </div>
+        </div>
+    );
 }
 
 export default App;
